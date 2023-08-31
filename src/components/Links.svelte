a90fcbd8c61229cf7c60194e94201590fd5f9ff5
Anton Fredriksson <79942428+Phoeenix05@users.noreply.github.com>
2023-08-31 20:07:27 +0300

    (feat) little bit of progress

---
+++ src/components/Links.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    const links = [
+        {
+            href: "/downloads",
+            title: "Download",
+        },
+        {
+            href: "https://github.com/phoeenix05/sakura.git",
+            title: "See on GitHub",
+        },
+        {
+            href: "https://github.com/phoeenix05/sakura-website.git",
+            title: "Code for this website",
+        },
+    ];
+</script>
+
+{#each links as link}
+    <div>
+        <a href={link.href}>{link.title}</a>
+    </div>
+{/each}
