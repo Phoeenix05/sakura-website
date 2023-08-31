82b755050372e0f8ad6754f995d2efb231f03d4d
Anton Fredriksson <79942428+Phoeenix05@users.noreply.github.com>
2023-08-31 19:13:55 +0300

    (fixed) config: stupid mistakes

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,9 @@ import tailwind from "@astrojs/tailwind";
 import { defineConfig } from 'astro/config';
 import vercel from "@astrojs/vercel/serverless";
 
-import netlify from "@astrojs/netlify/functions";
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [svelte(), tailwind(), mdx()],
   output: "server",
-  adapter: netlify()
+  adapter: vercel()
 });
