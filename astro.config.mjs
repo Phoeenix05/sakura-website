9b7ac6d98d6cf7fcd410a95f09e03a11246c5422
Anton Fredriksson <79942428+Phoeenix05@users.noreply.github.com>
2023-08-31 15:53:33 +0300

    (config) added deno adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config';
-
 import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 
+import deno from "@astrojs/deno";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte(), tailwind(), mdx()]
+  integrations: [svelte(), tailwind(), mdx()],
+  output: "server",
+  adapter: deno()
 });
