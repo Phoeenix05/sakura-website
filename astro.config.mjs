697896912795dc4136eb782198c1766bb980c4b7
Anton Fredriksson <79942428+Phoeenix05@users.noreply.github.com>
2023-08-31 19:03:58 +0300

    (???) ???

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,13 @@ import mdx from "@astrojs/mdx";
 import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from 'astro/config';
-
 import vercel from "@astrojs/vercel/serverless";
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [svelte(), tailwind(), mdx()],
   output: "server",
-  adapter: vercel()
+  adapter: netlify()
 });
